e0246b60367a3a50835d15baae54df1aa1cb2346
rishi-singh26 <rishisingh0831@gmail.com>
2024-08-24 21:41:28 +0530

    Updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HideImageComponent } from './features/hide-image/hide-image.component';
+import { ExtractImageComponent } from './features/extract-image/extract-image.component';
+import { HideTextComponent } from './features/hide-text/hide-text.component';
+import { ExtractTextComponent } from './features/extract-text/extract-text.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HideImageComponent },
+    { path: 'hide-image', component: HideImageComponent },
+    { path: 'extract-image', component: ExtractImageComponent },
+    { path: 'hide-text', component: HideTextComponent },
+    { path: 'extract-text', component: ExtractTextComponent },
+];
